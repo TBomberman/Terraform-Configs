8d2fe417d971fee0134ba7e0128ab9517a214adb
TBomberman <godwinwoo@hotmail.com>
2020-08-23 16:56:48 -0700

    Terraform configs.

---
+++ Dev/dev.tf
@@ -0,0 +1,33 @@
+variable "branch" {
+#  default = "master"
+  description = "This should match the branch tag on the ami image."
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-west-2"
+}
+
+data "aws_ami" "selected" {
+  owners   = ["self"]
+  filter {
+    name   = "tag:Build"
+    values = ["${var.branch}"]
+  }
+
+  most_recent = true
+}
+
+resource "aws_instance" "instance" {
+  ami = "${data.aws_ami.selected.id}"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "DEV ${var.branch}"
+  }
+}
+
+resource "aws_network_interface_sg_attachment" "sg_attachment" {
+  security_group_id    = "sg-079a7d9f8594516a7"
+  network_interface_id = aws_instance.instance.primary_network_interface_id
+}
